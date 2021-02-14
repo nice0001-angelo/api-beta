4d607fadcfa0ed639a48ad56e2b73b08254b4049
nice0001-angelo <nice0001@gmail.com>
2021-02-14 10:01:06 -0500

    Initial Update

---
+++ src/main/java/net/jin/ApiBetaApplication.java
@@ -0,0 +1,13 @@
+package net.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBetaApplication.class, args);
+	}
+
+}
