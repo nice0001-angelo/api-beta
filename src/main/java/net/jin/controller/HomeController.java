de826022d1f19543f1fc85f3418aa57ac732b280
nice0001-angelo <nice0001@gmail.com>
2021-02-14 12:10:25 -0500

    update

--- src/main/java/net/jin/controller/HomeController.java
+++ src/main/java/net/jin/controller/HomeController.java
@@ -7,6 +7,7 @@ import java.text.*;
 import java.util.*;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -17,12 +18,12 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(Locale locale, Model model) {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formatedDate = dateFormat.format(date);
-		model.addAttribute("Server Time")
+		model.addAttribute("serverTime", formatedDate);
 		
 		return "home";
 	}
