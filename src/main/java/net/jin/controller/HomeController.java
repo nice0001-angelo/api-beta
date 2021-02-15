ef58ee40d2039b010d65cfc341dbcd0d3f5de573
nice0001-angelo <nice0001@gmail.com>
2021-02-15 00:34:08 -0500

    update

--- src/main/java/net/jin/controller/HomeController.java
+++ src/main/java/net/jin/controller/HomeController.java
@@ -24,6 +24,7 @@ public class HomeController {
         Date date = new Date();
 		DateFormat dateFormat1 = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		DateFormat dateFormat2 = DateFormat.getDateInstance(DateFormat.FULL);
+		DateFormat dateFormat3 = DateFormat.getDateInstance(DateFormat.LONG);
 		
 		String formatedDate1 = dateFormat1.format(date);
 		System.out.println("formatedDate1 :"+formatedDate1);
@@ -33,6 +34,9 @@ public class HomeController {
 		System.out.println("formatedDate2 :"+formatedDate2);
 		model.addAttribute("serverTime2", formatedDate2);
 		
+		String formatedDate3 = dateFormat3.format(date);
+		System.out.println("formatedDate3 :"+formatedDate3);
+		model.addAttribute("serverTime3", formatedDate3);
 		
 		
 		return "home";
