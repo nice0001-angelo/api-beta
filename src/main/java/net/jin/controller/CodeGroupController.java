6d7898a918e80c0b1536ca2cbd8d888989b7310b
nice0001-angelo <nice0001@gmail.com>
2021-02-17 07:53:06 -0500

    update

--- src/main/java/net/jin/controller/CodeGroupController.java
+++ src/main/java/net/jin/controller/CodeGroupController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.java.*;
 import net.jin.domain.*;
+import net.jin.service.*;
 
 /**
  * @author njh
@@ -23,11 +24,20 @@ public class CodeGroupController {
 	@Autowired
 	private CodeGroupService service;
 	
+	//상세조회
 	@RequestMapping(value = "/{groupCode}", method = RequestMethod.GET)
 	public ResponseEntity<CodeGroup> read(@PathVariable("groupCode") String groupCode) throws Exception{
 		CodeGroup codeGroup = service.read(groupCode);
 		
 		return new ResponseEntity<>(codeGroup, HttpStatus.OK);
+	}
+	
+	//목록조회
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ResponseEntity<List<CodeGroup>> list() throws Exception{
+		log.info("list");
+		
+		return new ResponseEntity<>(service.list(), HttpStatus.OK)
 	}
 
 }
