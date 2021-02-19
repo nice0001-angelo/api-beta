2018a59de4f157c395a8ea6a67b2945b04784d0d
nice0001-angelo <nice0001@gmail.com>
2021-02-19 05:39:30 -0500

    update

--- src/main/java/net/jin/service/impl/CodeGroupServiceImpl.java
+++ src/main/java/net/jin/service/impl/CodeGroupServiceImpl.java
@@ -5,6 +5,7 @@ package net.jin.service.impl;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 
 import net.jin.domain.*;
@@ -17,34 +18,43 @@ import net.jin.service.*;
 @Service
 public class CodeGroupServiceImpl implements CodeGroupService{
 
+	@Autowired
+	CodeGroupMapper codeGroupMapper;
+	
+	//상세조회
 	@Override
 	public CodeGroup read(String groupCode) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return codeGroupMapper.read(groupCode);
 	}
 
+	//목록조회
 	@Override
 	public List<CodeGroup> list() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return codeGroupMapper.list();
 	}
 
+	//등록
 	@Override
 	public void register(CodeGroup codeGroup) throws Exception {
 		// TODO Auto-generated method stub
+		codeGroupMapper.create(codeGroup);
 		
 	}
 
+	//수정
 	@Override
 	public void modify(CodeGroup codeGroup) throws Exception {
 		// TODO Auto-generated method stub
+		codeGroupMapper.update(codeGroup);
 		
 	}
 
 	@Override
 	public void remove(String groupCode) throws Exception {
 		// TODO Auto-generated method stub
-		
+		codeGroupMapper.delete(groupCode);
 	}
 
 }
