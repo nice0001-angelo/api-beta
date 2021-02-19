c9eae6db372f3c40f5c650199c73ee1f3254626a
nice0001-angelo <nice0001@gmail.com>
2021-02-19 06:42:08 -0500

    update

--- src/main/java/net/jin/mapper/CodeGroupMapper.java
+++ src/main/java/net/jin/mapper/CodeGroupMapper.java
@@ -19,9 +19,13 @@ public interface CodeGroupMapper {
 	//목록조회
 	public List<CodeGroup> list() throws Exception;
 	
-	//
+	//등록
+	public void create(CodeGroup codeGroup) throws Exception;
 	
+	//수정
+	public void update(CodeGroup codeGroup) throws Exception;
 	
-	
+	//삭제
+	public void delete(String groupCode) throws Exception;
 	
 }
