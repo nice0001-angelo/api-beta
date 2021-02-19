c1740647fd03d48618fd7a30ec10b1349eba93be
nice0001-angelo <nice0001@gmail.com>
2021-02-19 07:35:18 -0500

    update

--- src/main/resources/static/js/app/code-group.js
+++ src/main/resources/static/js/app/code-group.js
@@ -1,16 +1,37 @@
-$(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url: "/codegroups",
-		contentType: "application/json; charset=UTF-8",
-		success: function(date){
-			console.log(date);
-			alert(JSON.stringify(data));
-		},
-		error: function(xhr, status, error){
-			alert("code:"+xhr.status+"\n"
-					+"message:"+xhr.responseText+"\n"
-					+"error:"+error);
-		}
-	});
+$(document).ready(function() {
+	
+			//목록조회
+			$("#codeGroupListBtn").on("click",function() {
+						$.ajax({
+							type : "GET",
+							url : "/codegroups",
+							contentType : "application/json; charset=UTF-8",
+							success : function(date) {
+								console.log(date);
+								alert(JSON.stringify(data));
+							},
+							error : function(xhr, status, error) {
+								alert("code:" + xhr.status + "\n" + "message:"
+										+ xhr.responseText + "\n" + "error:"
+										+ error);
+							}
+						});
+					});
+
+			//상세조회
+			$("#codeGroupReadBtn").on("click",function(){
+						$.ajax({
+							type : "GET",
+							url : "/codegroups/"+$("#groupCode").val(),
+							contentType : "application/jason; charset=UTF-8",
+							success : function(data){
+								console.log(data);
+							}
+							
+						});
+				
+				
+			});
+			
+
 });
