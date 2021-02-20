6eaf89376b5011704ab528719ad018e9979d8634
nice0001-angelo <nice0001@gmail.com>
2021-02-20 11:19:45 -0500

    update

--- src/main/resources/static/js/app/code-group.js
+++ src/main/resources/static/js/app/code-group.js
@@ -1,42 +1,24 @@
 $(document).ready(function() {
 	
-//			//목록조회
-//			$("#codeGroupListBtn").on("click",function() {
-//				alert("List");
-//						$.ajax({
-//							type : "GET",
-//							url : "/codegroups",
-//							contentType : "application/json; charset=UTF-8",
-//							success : function(date) {
-//								console.log(date);
-//								alert(JSON.stringify(data));
-//							},
-//							error : function(xhr, status, error) {
-//								alert("code:" + xhr.status + "\n" 
-//									+ "message:" + xhr.responseText + "\n" 
-//									+ "error:" + error);
-//							}
-//						});
-//					});
-	$("#codeGroupListBtn").on("click", function() {
-		
-		alert("List~~~!!");
-		$.ajax({
-			type : "GET",
-			url : "/codegroups",
-			contentType : "application/json; charset=UTF-8",
-			success : function(data) {
-				console.log(data);
-				
-				alert(JSON.stringify(data));
-			},
-			error : function(xhr, status, error) {
-				alert("code:" + xhr.status + "\n"
-					+ "message:" + xhr.responseText + "\n"
-					+ "error:" + error);
-			}
-		});
-	});
+			//목록조회
+			$("#codeGroupListBtn").on("click",function() {
+				alert("List~!!");
+						$.ajax({
+							type : "GET",
+							url : "/codegroups",
+							contentType : "application/json; charset=UTF-8",
+							success : function(data) {
+								console.log(data);
+								alert(JSON.stringify(data));
+							},
+							error : function(xhr, status, error) {
+								alert("code:" + xhr.status + "\n" 
+									+ "message:" + xhr.responseText + "\n" 
+									+ "error:" + error);
+							}
+						});
+					});
+
 			//상세조회
 			$("#codeGroupReadBtn").on("click",function(){
 						$.ajax({
