8618e45d0b6e9f5c3dfdb8a3ac9680e6337e3c2b
nice0001-angelo <nice0001@gmail.com>
2021-02-22 02:21:07 -0500

    update

---
+++ src/main/java/net/jin/controller/CodeDetailController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package net.jin.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.java.*;
+
+/**
+ * @author njh
+ *
+ */
+
+@Log
+@RestController
+@RequestMapping("/codedetails")
+public class CodeDetailController {
+	
+	
+
+}
---
+++ src/main/java/net/jin/domain/CodeLabelValue.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.jin.domain;
+
+import lombok.*;
+
+/**
+ * @author njh
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class CodeLabelValue {
+
+	private String label;
+	private String value;
+}
