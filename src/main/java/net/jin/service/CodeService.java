c00245b4ea9cb57e09d491aceff105fe2f1f1a07
nice0001-angelo <nice0001@gmail.com>
2021-02-23 02:30:37 -0500

    update

--- src/main/java/net/jin/service/CodeService.java
+++ src/main/java/net/jin/service/CodeService.java
@@ -3,10 +3,15 @@
  */
 package net.jin.service;
 
+import java.util.*;
+
+import net.jin.common.domain.*;
+
 /**
  * @author njh
  *
  */
 public interface CodeService {
 
+	public List<CodeLabelValue> getCodeGroupList() throws Exception;
 }
