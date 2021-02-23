aae91d1b33316cf5712c056cd48ddc88085bc6da
nice0001-angelo <nice0001@gmail.com>
2021-02-23 01:04:44 -0500

    update

--- src/main/java/net/jin/common/controller/CodeController.java
+++ src/main/java/net/jin/common/controller/CodeController.java
@@ -3,10 +3,24 @@
  */
 package net.jin.common.controller;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.java.*;
+import net.jin.service.*;
+
 /**
  * @author njh
  *
  */
+@Log
+@RestController
+@RequestMapping(value = "/codes")
 public class CodeController {
+	
+	@Autowired
+	private CodeService codeService;
+	
+	
 
 }
---
+++ src/main/java/net/jin/service/CodeService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package net.jin.service;
+
+/**
+ * @author njh
+ *
+ */
+public interface CodeService {
+
+}
