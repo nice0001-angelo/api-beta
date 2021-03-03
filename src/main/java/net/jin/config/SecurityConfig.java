c94d1b5fe9c1354ad0a3020c0214337273cc1df6
nice0001-angelo <nice0001@gmail.com>
2021-03-03 09:27:07 -0500

    update

--- src/main/java/net/jin/config/SecurityConfig.java
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -3,8 +3,12 @@
  */
 package net.jin.config;
 
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
 
 import lombok.extern.java.*;
 
@@ -22,5 +26,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		//CSRF 방지 지원 기능 활성화
 		httpSecurity.csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+	}
+	
+	@Bean
+	public PasswordEncoder createPasswordEncoder{
+		
+		return new BCryptPasswordEncoder();
+		
 	}
 }
---
+++ src/main/java/net/jin/domain/Member.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package net.jin.domain;
+
+import java.util.*;
+
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.annotation.*;
+
+import lombok.*;
+
+/**
+ * @author njh
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class Member {
+
+	private int userNo;
+	
+	@NotBlank
+	private String userId;
+	
+	@NotBlank
+	private String userPw;
+	
+	@NotBlank
+	private String userName;
+	
+	private String job;
+	
+	private int coin;
+	
+	private boolean enalbed;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date regDate;
+	
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date updDate;
+}
