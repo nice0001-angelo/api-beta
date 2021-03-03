da099a7d666fdb56904ed2238fc1323d8d43404b
nice0001-angelo <nice0001@gmail.com>
2021-03-02 21:00:06 -0500

    update

--- src/main/java/net/jin/config/SecurityConfig.java
+++ src/main/java/net/jin/config/SecurityConfig.java
@@ -3,10 +3,24 @@
  */
 package net.jin.config;
 
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+import lombok.extern.java.*;
+
 /**
  * @author njh
  *
  */
-public class SecurityConfig {
-	public
+@Log
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception{
+		log.info("Security Configuring....");
+		
+		//CSRF 방지 지원 기능 활성화
+		httpSecurity.csrf().disable();
+	}
 }
