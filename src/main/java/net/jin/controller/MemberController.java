919d139f9ebf1f30b98fdb727900cf284d64fd26
nice0001-angelo <nice0001@gmail.com>
2021-03-04 01:39:48 -0500

    update

---
+++ src/main/java/net/jin/controller/MemberController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.jin.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author njh
+ *
+ */
+@RestController
+@RequestMapping
+public class MemberController {
+
+}
