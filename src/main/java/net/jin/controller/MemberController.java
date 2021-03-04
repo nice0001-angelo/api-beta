40ae56e54ad2c05df9a3b31adaede508ae5c20c7
nice0001-angelo <nice0001@gmail.com>
2021-03-04 02:37:44 -0500

    update

--- src/main/java/net/jin/controller/MemberController.java
+++ src/main/java/net/jin/controller/MemberController.java
@@ -3,14 +3,24 @@
  */
 package net.jin.controller;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.java.*;
 
 /**
  * @author njh
  *
  */
+@Log
 @RestController
-@RequestMapping
+@RequestMapping(value = "/users")
 public class MemberController {
+	
+	@Autowired
+	private MemberService memberservice;
+	
+	//비밀번호 암호 처리기
+	private PasswordEncoder passwordEncoder = new BCriptPasswordEncoder();
 
 }
