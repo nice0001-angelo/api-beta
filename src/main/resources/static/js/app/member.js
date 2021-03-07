4459e27b5863320100de2c18497f671d3961dea7
nice0001-angelo <nice0001@gmail.com>
2021-03-07 07:24:49 -0500

    update

--- src/main/resources/static/js/app/member.js
+++ src/main/resources/static/js/app/member.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
 	
 	//member
-	//목록조회 List
-	$("#memberListBtn").on("click",function(){
+	//목록조회 list
+	$("#memberListBtn").on("Click",fucntion(){
+		alert("member listing~~~!!!")
 		$.ajax({
-		
+			type: "GET",
+			url: "/users",
+			contentType: "application/json; charset=UTF-8",
 			
 		});
-		
-		
-		
 	});
-	
-	
-	
-});//$(document).ready(function(){});
+});
