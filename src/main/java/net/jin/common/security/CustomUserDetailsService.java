e037b709f99fe803bf660e3cedb703789704b98b
nice0001-angelo <nice0001@gmail.com>
2021-04-14 22:04:56 -0400

    update

--- src/main/java/net/jin/common/security/CustomUserDetailsService.java
+++ src/main/java/net/jin/common/security/CustomUserDetailsService.java
@@ -3,10 +3,18 @@
  */
 package net.jin.common.security;
 
+import org.springframework.beans.factory.annotation.*;
+
+import lombok.extern.java.*;
+import net.jin.mapper.*;
+
 /**
  * @author njh
  *
  */
+@Log
 public class CustomUserDetailsService extends UserDatailsService {
 
+	@Autowired
+	public MemberMapper memberMapper; 
 }
