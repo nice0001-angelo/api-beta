8acea77c6f55ed26298f089999b72be5b9c97501
nice0001-angelo <nice0001@gmail.com>
2021-04-19 01:57:53 -0400

    update

--- src/main/java/net/jin/common/security/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/net/jin/common/security/jwt/filter/JwtAuthenticationFilter.java
@@ -3,6 +3,11 @@
  */
 package net.jin.common.security.jwt.filter;
 
+import javax.servlet.http.*;
+
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.*;
+
 import net.jin.common.security.jwt.constants.*;
 
 /**
@@ -21,5 +26,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		setFilterProcessesUrl(SecurityConstants.AUTH_LOGIN_URL);
 	}
  
-
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException {
+	
+		String username = httpServletRequest.getParameter("username");
+		String password = httpServletRequest.getParameter("password");
+		
+		Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+		
+		return authenticationManager.authenticate(authenticationToken);
+	}
 }
