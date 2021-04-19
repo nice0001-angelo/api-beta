25b2692c8770db59282142ce9697b51524ba3eb9
nice0001-angelo <nice0001@gmail.com>
2021-04-19 00:56:30 -0400

    update

--- src/main/java/net/jin/common/security/domain/CustomUser.java
+++ src/main/java/net/jin/common/security/domain/CustomUser.java
@@ -9,11 +9,24 @@ import net.jin.domain.*;
  * @author njh
  *
  */
-public class CustomUser {
+public class CustomUser extends User {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Member member;
+	
+	public CustomUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+		
+		super(username, password, authorities);
+		
+	}
+	
+	public CustomUser(Member member) {
+		super(member.getUserId(), member.getUserPw(), member.getMemberAuthList().stream()
+				.map(auth -> new SimpelGrantedAuthority(auth.getAuth())).collect(Collector.toList()));
+		
+		this.member = member;
+	}
 	
 	
 
