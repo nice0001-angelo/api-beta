92d2c736044b82d56ae2444746e0bd50de5c313c
nice0001-angelo <nice0001@gmail.com>
2021-04-20 17:01:00 -0400

    update

---
+++ src/main/java/net/jin/common/security/jwt/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.jin.common.security.jwt.filter;
+
+import org.mybatis.logging.*;
+import org.springframework.security.web.authentication.www.*;
+
+/**
+ * @author njh
+ *
+ */
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter{
+	
+	private static final Logger log = LoggerFactory.getLogger(JwtAuthorizationFilter.class);
+	
+	
+
+}
