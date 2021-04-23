8d1ea0881213e0a0343e926824af12087263ec95
nice0001-angelo <nice0001@gmail.com>
2021-04-23 04:26:33 -0400

    update

--- src/main/java/net/jin/common/util/AuthUtil.java
+++ src/main/java/net/jin/common/util/AuthUtil.java
@@ -3,10 +3,24 @@
  */
 package net.jin.common.util;
 
+import io.jsonwebtoken.*;
+import net.jin.common.security.jwt.constants.*;
+
 /**
  * @author njh
  *
  */
 public class AuthUtil {
 
+	//JWT 헤더값을 해석하여 사용자번호 획득
+	public static int getUserNo(String header) throws Exception{
+		String token = header.substring(7);
+		
+		byte[] signingKey = SecurityConstants.JWT_SECRET.getBytes();
+		
+		Jws<Claims> parseToken = Jwts.parser()
+				.setSigningKey(signingKey)
+				.parseClaimsJws(token);
+		
+	}
 }
