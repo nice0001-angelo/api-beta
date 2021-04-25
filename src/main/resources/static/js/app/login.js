fc53a13aa3dc1697d9f8f8152b723c44d2930b4c
nice0001-angelo <nice0001@gmail.com>
2021-04-25 09:56:23 -0400

    update

--- src/main/resources/static/js/app/login.js
+++ src/main/resources/static/js/app/login.js
@@ -2,16 +2,30 @@
 var ACCESS_TOKEN = "";
 
 $(document).ready(function(){
-	$.ajax({
-		type: "POST",
-		url: "aou/authenticate?username="+$("#userid").val()+"&password="+$("#password").val(),
-		success: function(data, textStatus, request){
-			var responseHeader = request.getResponseHeader("Authorization");
-			alert(responseHeader);
-		}
+	//로그인
+	$("#loginBtn").on("click",function(){
+		$.ajax({
+			type: "POST",
+			url: "aou/authenticate?username="+$("#userid").val()+"&password="+$("#password").val(),
+			success: function(data, textStatus, request){
+				var responseHeader = request.getResponseHeader("Authorization");
+				alert(responseHeader);
+				
+				ACCESS_TOKEN = responseHeader.substr(7);
+				
+				console.log(ACCESS_TOKEN);
+			}
+			error: function(request, textStatus, errorThrown){
+				alert(request.getREsponseHeader("Authorization"));
+			}
+			
+		});
+	});
+	
+	//로그인 사용자 정보
+	$("#myInfoBtn").on("click",function(){
 		
 		
 	});
-	
 	
 });
