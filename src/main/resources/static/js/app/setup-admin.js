06d4cdf5c86ec7307b044e4510421591e84b9fd5
nice0001-angelo <nice0001@gmail.com>
2021-04-30 03:10:40 -0400

    update

--- src/main/resources/static/js/app/setup-admin.js
+++ src/main/resources/static/js/app/setup-admin.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	//등록
+	$("#adminRegisterBtn").on("click",function(){
+		
+		
+	});
 	
+	//입력값 리셋
+	$("#adminReset").on("click",function(){
+		$("#adminId").val("");
+		$("#adminId").val("");
+		$("#adminId").val("");
+		
+	});
 	
 });
