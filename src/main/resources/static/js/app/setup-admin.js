ae719cc4bb4303cb0e85bd09260a5533de0bbb8e
nice0001-angelo <nice0001@gmail.com>
2021-04-30 03:26:41 -0400

    update

--- src/main/resources/static/js/app/setup-admin.js
+++ src/main/resources/static/js/app/setup-admin.js
@@ -11,7 +11,21 @@ $(document).ready(function(){
 		
 		alert("userObject: "+JSON.stringnify(userObject);
 		
-		
+		$.ajax({
+			type: "POST",
+			url: "/users/setup",
+			success: function(data, textStatus, request){
+				var responseHeader = request.getResponseHeader("Authorization");
+				alert(responseHeader);
+				
+				ACCESS_TOKEN = responseHeader.substr(7);
+				
+				console.log(ACCESS_TOKEN);
+			},
+			error: function(request, textStatus, errorThrown){
+				alert(request.getResponseHeader("Authorization"));
+			}
+		});
 		
 	});
 	
